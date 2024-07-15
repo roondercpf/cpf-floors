b690214ebaa182a8d78af0b5a679dfff35a63a97
kevin colmenares <kevincolmenaresm@gmail.com>
2024-07-15 14:50:27 -0400

    [Beta0.2]: add env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "cpfmainbucket.s3.amazonaws.com"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
