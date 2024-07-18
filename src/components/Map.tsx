923f6932f238ce5acca1731afbd16ea57319c07d
Alfredo <offsound2020@gmail.com>
2024-07-18 12:42:04 -0400

    [Beta v0.9] Dealer locator map component update and styling

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { ReactNode, useRef, useMemo } from "react";
-import DealerMap, { Marker, Popup } from "react-map-gl";
+import Image from "next/image";
+import DealerMap, {
+  Marker,
+  Popup,
+  NavigationControl,
+  GeolocateControl,
+  FullscreenControl,
+  ScaleControl,
+
+} from "react-map-gl";
 import { Dealerlocator } from "@/interfaces/dealer-locator.model";
 import { getCenter } from "geolib";
-import "mapbox-gl/src/css/mapbox-gl.css";
+import "@/app/mapbox-gl.css";
 
 interface MapProps {
   props?: ReactNode;
@@ -24,29 +33,53 @@ export const Map = ({ data, ...props }: MapProps) => {
   );
 
   return (
-    <DealerMap
-      mapboxAccessToken={`${process.env.NEXT_PUBLIC_ACCESSTOKEN}`}
-      mapStyle={"mapbox://styles/kevyuppie/clyndbndm01y601nzavka2oe9"}
-      initialViewState={{
-        ...center,
-        zoom: 5.6,
-        bearing: 0,
-        pitch: 0,
-      }}
-    >
-      {data.map((dealer, index) => {
-        return (
-          <Marker
-            key={index}
-            latitude={dealer.lat}
-            longitude={dealer.lng}
-            color="red"
-            anchor="bottom"
-            offsetTop={-20}
-            offsetLeft={-7}
-          ></Marker>
-        );
-      })}
-    </DealerMap>
+    <div className="map-container">
+        {/* <div className="map-container-bar">
+          <div className="map-title-bar text-center">
+            <Image
+              className="mb-10"
+              src="/footer-logo.svg"
+              height={150}
+              width={150}
+              alt="CPF Logo"
+            ></Image>
+            <h2>Find your Nearest Dealer</h2>
+            <input
+              placeholder="Enter your dealer Name"
+              type="search"
+              name="dealer"
+              id="dealer"
+            />
+          </div>
+        </div> */}
+      <DealerMap
+        mapboxAccessToken={`${process.env.NEXT_PUBLIC_ACCESSTOKEN}`}
+        mapStyle={"mapbox://styles/kevyuppie/clyrfqie902ej01p88zlb82xz"}
+        initialViewState={{
+          ...center,
+          zoom: 5.6,
+          bearing: 0,
+          pitch: 0,
+        }}
+      >
+        <NavigationControl position="top-left" />
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <ScaleControl />
+        {data.map((dealer, index) => {
+          return (
+            <Marker
+              key={index}
+              latitude={dealer.lat}
+              longitude={dealer.lng}
+              color="black"
+              anchor="bottom"
+              offsetTop={-20}
+              offsetLeft={-7}
+            ></Marker>
+          );
+        })}
+      </DealerMap>
+    </div>
   );
 };
