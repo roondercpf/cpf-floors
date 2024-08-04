169c83ab322562c6083589848b88178193b17fdc
Alfredo <offsound2020@gmail.com>
2024-08-04 08:42:34 -0400

    [Alfa] - Footer mobile component

--- src/components/BlogArticleCarousel.tsx
+++ src/components/BlogArticleCarousel.tsx
@@ -1,5 +1,3 @@
-
-
 import { useCallback } from "react";
 import type { Metadata } from "next";
 import { FRONTEND } from "@/utils/env";
@@ -30,32 +28,59 @@ async function BlogArticleCarousel() {
 
   return (
     <>
-      <div className="blog-card-container">
-        {blogs.length > 0 ? (
-          <>
-            {blogs.map((blog) => {
-              return (
-                <div key={blog._id} className="blog-card">
-                  <Image
-                    src={blog.featured_image}
-                    alt={blog.title}
-                    width={200}
-                    height={200}
-                  />
-                  <h2>{blog.title}</h2>
-                  <h3>{blog.subtitle}</h3>
-                  <Link className="link-dark" href={`/blogs/${blog._id}`}>
-                    {"Read More"}
-                  </Link>
-                </div>
-              );
-            })}
-          </>
-        ) : (
-          <>
-            <h2>Not Found</h2>
-          </>
-        )}
+      <div className="embla">
+        <div className="embla__viewport">
+          <div className="embla__container">
+            <div className="embla__slide">
+              <div className="blog-card-container">
+                {blogs.length > 0 ? (
+                  <>
+                    {blogs.map((blog) => {
+                      return (
+                        <div key={blog._id} className="blog-card">
+                          <Image
+                            src={blog.featured_image}
+                            alt={blog.title}
+                            width={200}
+                            height={200}
+                          />
+                          <h2>{blog.title}</h2>
+                          <Link
+                            className="link-dark"
+                            href={`/blogs/${blog._id}`}
+                          >
+                            {"Read More"}
+                          </Link>
+                        </div>
+                      );
+                    })}
+                  </>
+                ) : (
+                  <>
+                    <h2>Article Not Found</h2>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/*BUTTONS*/}
+
+        <div className="home-carousel-buttons">
+          <button
+            className="embla__prev home-carousel-prev"
+            onClick={scrollPrev}
+          >
+            <Image src="/prev.svg" height={50} width={50} alt="prev"></Image>
+          </button>
+          <button
+            className="embla__next home-carousel-next"
+            onClick={scrollNext}
+          >
+            <Image src="/next.svg" height={50} width={50} alt="next"></Image>
+          </button>
+        </div>
       </div>
     </>
   );
---
+++ src/components/FooterMobile.tsx
@@ -0,0 +1,118 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import "@/app/sass/FooterMobile.scss";
+
+export default function FooterMobile() {
+  return (
+    <>
+      <div className="footer-mobile p-10 py-20">
+        <div className="footer-social flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src="/footer-logo.svg"
+              height={80}
+              width={80}
+              alt="CPF Floors logo"
+            ></Image>
+          </Link>
+          <div className="footer-icons flex">
+            <Link href="#">
+              <Image
+                className="mx-4"
+                src="/instagram.svg"
+                height={25}
+                width={25}
+                alt="CPF Floors logo"
+              ></Image>
+            </Link>
+            <Link href="#">
+              <Image
+                className="mx-4"
+                src="/pinterest.svg"
+                height={25}
+                width={25}
+                alt="CPF Floors logo"
+              ></Image>
+            </Link>
+            <Link href="#">
+              <Image
+                className="mx-4"
+                src="/linkedin.svg"
+                height={25}
+                width={25}
+                alt="CPF Floors logo"
+              ></Image>
+            </Link>
+          </div>
+        </div>
+        <h2>Let&apos;s click together</h2>
+
+        {/*ACCORDION */}
+
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="font-bold py-10">
+              <h3>Our Products</h3>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col py-10">
+              <Link href="/collections/vinyl">Vinyl</Link>
+              <Link href="/collections/laminate">Laminate</Link>
+              <Link href="/corepad">Underlayment</Link>
+              <Link href="/core-moldings">Moldings</Link>
+              <Link href="/coresteps">Steps</Link>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-2">
+            <AccordionTrigger className="font-bold py-10">
+              <h3>Resources</h3>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link href="/display-resources">Display Resources</Link>
+              <Link href="/financing">Financing</Link>
+              <Link href="/faq">F.A.Q.</Link>
+              <Link href="/privacy-policy">Privacy Policy</Link>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-3">
+            <AccordionTrigger className="font-bold py-10">
+              <h3>Get Inspired</h3>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link href="/blogs">Blog</Link>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-4">
+            <AccordionTrigger className="font-bold py-10">
+              <h3>Our Company</h3>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link href="/become-a-dealer">Become a Dealer</Link>
+              <Link href="/dealer-locator">Dealer Locator</Link>
+              <Link href="/about-us">About Us</Link>
+              <Link href="/contact-us">Contact Us</Link>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        <div className="logos flex flex-wrap  flex-row items-center">
+            <Image className="my-5" src="/footer1.svg" height={30} width={30} alt="CPF Floors parters"></Image>
+            <Image className="my-5" src="/footer2.svg" height={30} width={30} alt="CPF Floors parters"></Image>
+            <Image className="my-5" src="/wfca.svg" height={30} width={30} alt="CPF Floors parters"></Image>
+            <Image className="bluetape my-5" src="/financing/bluetape.svg" height={30} width={30} alt="CPF Floors parters"></Image>
+        </div>
+        <p className="font-semibold">© 2024 CPF Floors. All rights reserved.</p>
+      </div>
+    </>
+  );
+}
