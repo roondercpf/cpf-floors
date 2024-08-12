dc5741f6a6933806310e99b01c7a8ceb2924cb2a
Alfredo <offsound2020@gmail.com>
2024-08-12 14:52:30 -0400

    [Alpha] css styles update

--- src/app/order-samples/page.tsx
+++ src/app/order-samples/page.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import { motion } from "framer-motion"
 
 import "@/app/sass/OrderSample.scss";
 
@@ -25,12 +27,15 @@ export default function OrderSample() {
           </Link>
         </div>
         <div className="order-img">
-          <Image
+          <motion.img
             src="/order-sample/banner-img.png"
             height={600}
             width={600}
             alt="CPF Floors Resources"
-          ></Image>
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 1}}
+          ></motion.img>
         </div>
       </div>
 
@@ -42,32 +47,44 @@ export default function OrderSample() {
           <h3>Discover the endless possibilities <br /> you&apos;ll have as a dealer!</h3>
           <p>Access to exclusive support and resources.</p>
         </div>
-        <div className="order-cards-container flex flex-row flex-wrap justify-center items-center">
+        <div className="order-cards-container flex flex-row flex-wrap justify-center items-start">
           <div className="order-card">
-            <Image src="/order-sample/card1.png" height={200} width={200} alt="CPF Order a Sample"></Image>
+            <motion.img src="/order-sample/card1.png" height={200} width={200} alt="CPF Order a Sample"
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1,}}
+            transition={{duration: 1}}></motion.img>
             <h3>Regular sample</h3>
             <p>Discover the strength and durability of our Strapset. Order a sample to see firsthand why it&apos;s a top choice for professionals.</p>
           </div>
           <div className="order-card">
-            <Image src="/order-sample/card2.png" height={200} width={200} alt="CPF Order a Sample"></Image>
+            <motion.img src="/order-sample/card2.png" height={200} width={200} alt="CPF Order a Sample"
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1,}}
+            transition={{duration: 1}}></motion.img>
             <h3>Strap Set</h3>
             <p>Get a closer look at our product quality with a regular sample. Perfect for evaluating color, texture, and durability.</p>
           </div>
           <div className="order-card">
-            <Image src="/order-sample/card3.png" height={200} width={200} alt="CPF Order a Sample"></Image>
+            <motion.img src="/order-sample/card3.png" height={200} width={200} alt="CPF Order a Sample"
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1,}}
+            transition={{duration: 1}}></motion.img>
             <h3>Architech Folder</h3>
             <p>Equip your sales team with professional-grade materials. Order an Architect Folder filled with product samples and information.</p>
           </div>
         </div>
       </div>
 
-      <div className="order-last-banner-container">
+      <motion.div className="order-last-banner-container"
+      initial={{opacity: 0, y: 800}}
+      animate={{opacity: 1, y: 0}}
+      >
         <div className="last-banner-container flex flex-col justify-center items-center">
           <h3>Join our dealer network</h3>
           <p>Order your samples and get started</p>
           <Link className="link-light" href="/contact-us">Contact Now!</Link>
         </div>
-      </div>
+      </motion.div>
 
 
 
