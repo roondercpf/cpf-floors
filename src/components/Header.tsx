59eb81805c0551cbcf4dea2bf4f22b044084e8de
Alfredo <offsound2020@gmail.com>
2024-10-23 17:15:08 -0400

    [Alpha] - Mobile nav update & styles css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -353,83 +353,59 @@ function Header() {
                     <AccordionTrigger>
                       <h3>Products</h3>
                     </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        We provide a wide range of rigid core vinyl and laminate
-                        flooring. Our selection caters to both residential and
-                        commercial needs, and includes all the necessary
-                        accesories.
-                      </p>
-                    </AccordionContent>
+                    <div className="link-container">
+                      <AccordionContent className="link-container-list flex flex-col p-5">
+                        {list?.collections.map((link, index) => (
+                          <Link
+                            className="mb-5 text"
+                            onClick={() => {
+                              setOpenProducts(false);
+                              setMobileNav(false);
+                            }}
+                            key={index}
+                            href={`/collection/${link.collection_url}`}
+                          >
+                            {link.name}
+                          </Link>
+                        ))}
+                      </AccordionContent>
+                    </div>
                   </AccordionItem>
-
+                  <div className="link-container-normal py-5">
+                    <Link href="/dealer-locator">Dealer Locator</Link>
+                  </div>
+                  <div className="link-container-normal py-5">
+                    <Link href="/become-a-dealer">Become a Dealer</Link>
+                  </div>
+                  <div className="link-container-normal py-5">
+                    <Link href="/about-us">About Us</Link>
+                  </div>
                   <AccordionItem value="item-2">
                     <AccordionTrigger>
-                      <h3>Do you sell to individual homeowners?</h3>
+                      <h3>Get Inspired</h3>
                     </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        While our primary clientele consists of retailers,
-                        builders, and contractors, we also serve individual
-                        homeowners. Feel free to explore our products and submit
-                        a quote.
-                      </p>
-                    </AccordionContent>
+                    <div className="link-container">
+                      <AccordionContent className="link-container-list flex flex-col p-5">
+                        <Link href="/blog">Our Blog</Link>
+                      </AccordionContent>
+                    </div>
                   </AccordionItem>
-
-                  <AccordionItem value="item-3">
-                    <AccordionTrigger>
-                      <h3>Do you offer installation services?</h3>
-                    </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        While we don&apos;t provide installation ourselves, we
-                        can recommend trusted professionals in your area in the
-                        Dealer Locator section. Feel free to ask for referrals!
-                      </p>
-                    </AccordionContent>
-                  </AccordionItem>
-
-                  <AccordionItem value="item-4">
-                    <AccordionTrigger>
-                      <h3>Is there a warranty on your products?</h3>
-                    </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        Yes! Our warranties covers Coreproof, Quicksurfaces, and
-                        Finfloor brands. You can find detailed warranty
-                        information for each product on the Technical
-                        Specifications section.
-                      </p>
-                    </AccordionContent>
-                  </AccordionItem>
-
-                  <AccordionItem value="item-5">
-                    <AccordionTrigger>
-                      <h3>How can I place an order?</h3>
-                    </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        You can reach out to our team directly on the Become A
-                        Dealer section. We&apos;ll guide you through the ordering
-                        process and assist with any questions.
-                      </p>
-                    </AccordionContent>
-                  </AccordionItem>
-
-                  <AccordionItem value="item-6">
-                    <AccordionTrigger>
-                      <h3>Can I request samples before making a decision?</h3>
-                    </AccordionTrigger>
-                    <AccordionContent className="py-0">
-                      <p>
-                        Absolutely! Contact us, and we&apos;ll arrange your free
-                        samples of our flooring products. It&apos;s essential to see
-                        and feel the materials firsthand.
-                      </p>
-                    </AccordionContent>
-                  </AccordionItem>
+                  <div className="link-container-normal py-5">
+                    <Link href="/order-samples">Order Samples</Link>
+                  </div>
+                  <div className="link-container-normal py-5">
+                    <Link href="/contact-us">Contact Us</Link>
+                  </div>
                 </Accordion>
+                <div className="flex justify-center items-center py-20">
+                  <Image
+                    src="/footer-logo.svg"
+                    height={140}
+                    width={140}
+                    alt="CPF Floors"
+                    unoptimized
+                  ></Image>
+                </div>
               </div>
             </motion.div>
           )}
